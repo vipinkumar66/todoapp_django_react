ec0864520036a3b9c06c0c92c03564ff58ff8c1c
vipin <vipinkumarlamba55@gmail.com>
2023-04-09 23:33:38 +0530

    django model setup

--- server/api/models.py
+++ server/api/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Note(models.Model):
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- server/api/views.py
+++ server/api/views.py
@@ -3,4 +3,30 @@ from django.http import JsonResponse
 
 # Create your views here.
 def getRoutes(request):
-    return JsonResponse("our first api", safe=False)
+    routes = [
+        {
+            'endpoint':'/notes/',
+            'method':'GET',
+            'body': None,
+            'description':'Returns an array of list of notes'
+        },
+        {
+            'endpoint':'/notes/id',
+            'method':'GET',
+            'body': None,
+            'description':'Returns info about a single note'
+        },
+        {
+            'endpoint':'/notes/create/',
+            'method':'POST',
+            'body': {'body':""},
+            'description':'create a note with the given details in the body'
+        },
+        {
+            'endpoint':'notes/delete/id',
+            'method':'GET',
+            'body': None,
+            'description':'delete the note with the given id'
+        }
+    ]
+    return JsonResponse(routes, safe=False)
