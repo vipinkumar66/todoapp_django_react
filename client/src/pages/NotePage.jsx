66a45a16092611f2f5cc025e12d6c40d9dffdb55
vipin <vipinkumarlamba55@gmail.com>
2023-04-12 23:39:14 +0530

    Individual Note page view and Link

--- client/src/pages/NotePage.jsx
+++ client/src/pages/NotePage.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const NotePage = () => {
     const {id} = useParams()
+    const [noteData, setNoteData] = useState(null)
+
+    useEffect(() => {
+        const singleNote = async() =>{
+            try{
+                let response = await fetch(`http://127.0.0.1:8000/home/notes/${id}/`)
+                let data = await response.json()
+                setNoteData(data)
+            }catch(error){
+            console.log(error)
+            }
+        }
+        singleNote()
+    },[])
+
   return (
     <div>
-      <h2>Single note {id}</h2>
+      <h2>{noteData? noteData.body : ""}</h2>
+      <p>{noteData ? noteData.created : ""}</p>
+      <p>{noteData ? noteData.updated: ""}</p>
     </div>
   )
 }
