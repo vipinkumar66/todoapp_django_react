65dd60e50346414540384e46c5394a1ca2b1a950
vipin <vipinkumarlamba55@gmail.com>
2023-04-12 22:21:02 +0530

    fixing cors error

--- client/src/pages/NotesListPage.jsx
+++ client/src/pages/NotesListPage.jsx
@@ -9,7 +9,6 @@ const NotesListPage = () => {
         let response = await fetch('http://127.0.0.1:8000/home/notes/')
         let all_notes = await response.json()
         setNotes(all_notes)
-        console.log(notes)
       } catch(error){
         console.log(error)
       }
@@ -20,6 +19,15 @@ const NotesListPage = () => {
   return (
     <div>
       <h2>ALL NOTES</h2>
+      {notes.map((note) => {
+        return(
+          <div key={note.id}>
+            <h3>
+            {note.body}
+            </h3>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- server/server/settings.py
+++ server/server/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -123,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
