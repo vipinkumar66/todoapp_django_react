8727d0516b272e7a1e4ba0cb9dc931599324cc3c
vipin <vipinkumarlamba55@gmail.com>
2023-04-12 20:53:07 +0530

    trying fetch in react

--- client/src/pages/NotesListPage.jsx
+++ client/src/pages/NotesListPage.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const NotesListPage = () => {
+
+  let [notes, setNotes] = useState([])
+  useEffect(()=>{
+    const getData = async () =>{
+      try{
+        let response = await fetch('http://127.0.0.1:8000/home/notes/')
+        let all_notes = await response.json()
+        setNotes(all_notes)
+        console.log(notes)
+      } catch(error){
+        console.log(error)
+      }
+    }
+    getData()
+  },[])
+
   return (
     <div>
       <h2>ALL NOTES</h2>
